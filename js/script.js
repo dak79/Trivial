4ea8fe63d204c5b036abdebea0278a118129585a
dak79 <danielecampari@gmail.com>
2021-01-18 16:24:05 +0300

    small fixing

--- js/script.js
+++ js/script.js
@@ -9,67 +9,54 @@ function right() {
     let green = document.querySelector('.rightChoice');
     green.style.color = 'green';
     green.innerHTML += '&#x2713; + 3 POINT';
-
     for (let i = 1; i <= 4; i++) {
       document.querySelector('.all' + i).disabled = true;
     }
-
     point += 3;
     document.querySelector('#point').innerHTML = point;
-
     if (document.querySelector('.warning') !== '') {
       let warning = document.querySelector('.warning');
       warning.innerHTML = '';
     }
-
     return true;
 
   } else if (document.querySelector('.wrongBox1').checked === true) {
     let red = document.querySelector('.wrongChoice1');
     red.style.color = 'red';
     red.style.textDecoration = 'line-through';
-
     for (let i = 1; i <= 4; i++) {
       document.querySelector('.all' + i).disabled = true;
     }
-
     if (document.querySelector('.warning') !== '') {
       let warning = document.querySelector('.warning');
       warning.innerHTML = '';
     }
-
     return true;
 
   } else if (document.querySelector('.wrongBox2').checked === true) {
     let red = document.querySelector('.wrongChoice2');
     red.style.color = 'red';
     red.style.textDecoration = 'line-through';
-
     for (let i = 1; i <= 4; i++) {
       document.querySelector('.all' + i).disabled = true;
     }
-
     if (document.querySelector('.warning') !== '') {
       let warning = document.querySelector('.warning');
       warning.innerHTML = '';
     }
-
     return true;
 
   } else if (document.querySelector('.wrongBox3').checked === true) {
     let red = document.querySelector('.wrongChoice3');
     red.style.color = 'red';
     red.style.textDecoration = 'line-through';
-
     for (let i = 1; i <= 4; i++) {
       document.querySelector('.all' + i).disabled = true;
     }
-
     if (document.querySelector('.warning') !== '') {
       let warning = document.querySelector('.warning');
       warning.innerHTML = '';
     }
-
     return true;
   }
 }
@@ -84,18 +71,15 @@ function next() {
     let questionOne = document.querySelector('.displayQuestion' + i);
     questionOne.style.display = 'flex';
     questionOne.style.flexFlow = 'column';
-
     if (qNumb === 10) {
       let questionNumber = document.querySelector('#questionNumber');
       questionNumber.innerHTML = '10/10';
       let totalPoint = document.querySelector('#total');
       totalPoint.innerHTML = point + "/30";
-
     } else {
       qNumb += 1;
       let questionNumber = document.querySelector('#questionNumber');
       questionNumber.innerHTML = qNumb + '/ 10';
-
     }
 
   } else {
@@ -112,7 +96,6 @@ function selectOnlyThis(id) {
   for (var i = 1; i <= 4; i++) {
     document.querySelector('.all' + i).checked = false;
   }
-
   document.getElementById(id).checked = true;
 }
 
